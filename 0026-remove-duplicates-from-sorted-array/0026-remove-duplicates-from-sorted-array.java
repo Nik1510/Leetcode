b4831f08fcf00de6058590c239cfd7c2a107ec3b
Nikhil Shaw <Nikhilshaw575@gmail.com>
2024-11-14 18:01:56 +0530

    Time: 0 ms (100%), Space: 44.8 MB (44.23%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // i will always point to unqiue element 
-        // and j will get the unqiue element from the array and give it to i 
        int i=0; int j =1;
        while(j<nums.length){
         if(nums[i]!=nums[j]){
-            // got a unique element 
-            // i will make space
             i++;
             nums[i]=nums[j];
         }
