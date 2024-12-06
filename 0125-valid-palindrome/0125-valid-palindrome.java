0d41659d3145214fe1befdda445294e928f4f32f
Nikhil Shaw <Nikhilshaw575@gmail.com>
2024-12-06 21:14:12 +0530

    Time: 4 ms (52.32%), Space: 43.4 MB (50.03%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder str = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)){
+                str.append(Character.toLowerCase(ch));
+            }
+        }
+        int i =0; 
+        int j = str.length()-1;
+
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
