d08485d93801bc5033cfc864509dbda1dc61e137
Nikhil Shaw <Nikhilshaw575@gmail.com>
2024-12-13 21:50:31 +0530

    Time: 1 ms (90.83%), Space: 45.1 MB (31.37%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,33 +15,28 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-     List<List<Integer>> result = new ArrayList<>();
-
+        List<List<Integer>> result = new ArrayList<>();
         if(root ==null){
             return result;
         }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
 
-     Queue<TreeNode> queue = new LinkedList<>();
-
-     queue.offer(root);
-     while(!queue.isEmpty()){
-        int levelSize = queue.size();
-        List<Integer> currentLevel = new ArrayList<>();
-
-        for(int i =0; i<levelSize;i++){
-            TreeNode currentNode = queue.poll();
-            currentLevel.add(currentNode.val);
-
-            if(currentNode.left!=null){
-                queue.offer(currentNode.left);
+            for(int i =0; i<levelSize;i++){
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if(currentNode.left!=null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right!=null){
+                    queue.offer(currentNode.right);
+                }
             }
-
-            if(currentNode.right!=null){
-                queue.offer(currentNode.right);
-            }
+            result.add(currentLevel);
         }
-        result.add(currentLevel);
-     }   
-     return result;
+        return result;
     }
 }
