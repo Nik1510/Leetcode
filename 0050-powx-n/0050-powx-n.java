40c11f03fe7699ef38d59010de83c8e8f214981c
Nikhil Shaw <Nikhilshaw575@gmail.com>
2025-01-29 11:32:30 +0530

    Time: 0 ms (100%), Space: 42.6 MB (29.9%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        return  Math.pow(x,n);
+        return solve(x, (long) n);
+    }
+
+    double solve(double x, long n) {
+        if (x == 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        } else if (n < 0) {
+            return solve(1 / x, -n);
+        } else if (n % 2 == 0) {
+            return solve(x * x, n / 2);
+        } else {
+            return x * solve(x * x, (n - 1) / 2);
+        }
     }
 }
