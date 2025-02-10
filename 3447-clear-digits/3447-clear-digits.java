dc7562d3b6abe4ec6c1b61511148e01c34b23a2a
Nikhil Shaw <Nikhilshaw575@gmail.com>
2025-02-10 18:25:05 +0530

    Time: 3 ms (38.21%), Space: 42.6 MB (60.2%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                if (!stack.isEmpty()) {
+                    stack.pop(); 
+                }
+            } else {
+                stack.push(ch); 
+            }
+        }
+        
+        StringBuilder str = new StringBuilder();
+        while (!stack.isEmpty()) {
+            str.append(stack.pop());
+        }
+
+        return str.reverse().toString(); 
+    }
+}
